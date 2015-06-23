2ab5acef5ae914f81aa500a2f18b6192ce946955
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-06-23 16:21:53 -0700

    Pass in value so log statement prints

--- app/src/main/java/io/github/jrrembert/sunshine/app/ForecastFragment.java
+++ app/src/main/java/io/github/jrrembert/sunshine/app/ForecastFragment.java
@@ -54,7 +54,7 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute();
+            weatherTask.execute("98126");
             return true;
         }
         return super.onOptionsItemSelected(item);
