300b2963909e890e700778a6a84d50ff4cc0fa42
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-06-26 10:54:34 -0700

    Launch SettingsActivity when Settings button is pressed

--- app/src/main/java/io/github/jrrembert/sunshine/app/MainActivity.java
+++ app/src/main/java/io/github/jrrembert/sunshine/app/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.jrrembert.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -34,9 +35,9 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
